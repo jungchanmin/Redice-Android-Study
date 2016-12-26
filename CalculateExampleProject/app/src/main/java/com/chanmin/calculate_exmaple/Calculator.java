82b5212a7dad1dd7892dafcaa3f77af8f83ef3b2
jungchanmin <pgy6072@gmail.com>
2016-12-26 14:54:26 +0900

    연산기능 추가

--- CalculateExampleProject/app/src/main/java/com/chanmin/calculate_exmaple/Calculator.java
+++ CalculateExampleProject/app/src/main/java/com/chanmin/calculate_exmaple/Calculator.java
@@ -36,7 +36,7 @@ public class Calculator extends AppCompatActivity {
         backOperator.clear();
         operationStack.clear();
         alignedOperator.clear();
-        String value = sendingText.getText().toString();
+        String value;
         value = "";
         sendingText.setText(value);
     }
@@ -45,27 +45,24 @@ public class Calculator extends AppCompatActivity {
     }
 
     public void equalClicked(View v) {
-        if(isNumberPadClick){
+        if (isNumberPadClick) {
             String tempNumber = "";
-            for(String operator : backOperator){
-                if(operator.equals("/") || operator.equals("*")){
+            for (String operator : backOperator) {
+                if (operator.equals("/") || operator.equals("*")) {
                     operationStack.push(operator);
                     alignedOperator.add(tempNumber);
                     tempNumber = "";
-                } else if(operator.equals("+") || operator.equals("-")){
-                    System.out.println("+ or - operator detected");
+                } else if (operator.equals("+") || operator.equals("-")) {
+                    //System.out.println("+ or - operator detected");
                     alignedOperator.add(tempNumber);
                     tempNumber = "";
                     String multiplyCheck;
-                    for(;;){
-                        if(operationStack.empty()){
-                            break;
-                        }
+                    while (!operationStack.empty()) {
                         multiplyCheck = operationStack.pop();
-                        System.out.println("multiplyCheck:"+multiplyCheck);
-                        if(multiplyCheck.equals("*") || multiplyCheck.equals("/")){
+                        //System.out.println("multiplyCheck:"+multiplyCheck);
+                        if (multiplyCheck.equals("*") || multiplyCheck.equals("/")) {
                             alignedOperator.add(multiplyCheck);
-                        }else{
+                        } else {
                             operationStack.push(multiplyCheck);
                             break;
                         }
@@ -76,29 +73,97 @@ public class Calculator extends AppCompatActivity {
                 }
             }
             alignedOperator.add(tempNumber);
-            int count = 0;
+            //int count = 0;
             String stack;
-            for(;;){
-                System.out.println("연산자 스택에서 뺴내기. count : " + count);
-                count++;
-                if(operationStack.empty()){
+            for (; ; ) {
+                //System.out.println("연산자 스택에서 뺴내기. count : " + count);
+                //count++;
+                if (operationStack.empty()) {
                     break;
                 }
                 stack = operationStack.pop();
                 alignedOperator.add(stack);
             }
-            String value  = "";
+            /*String value  = "";
             for(String result : alignedOperator){
                 value += result;
             }
-            sendingText.setText(value);
+            sendingText.setText(value);*/
+            String tempValue;
+            String stringNumber1 = "";
+            String stringNumber2 = "";
+            int firstNumberPoint = 0;
+            float tempResult = 0;
+            int alignedOperatorSize = alignedOperator.size();
+            for (int i = 0; i < alignedOperatorSize; i++) {
+                System.out.println("i:" + i);
+                tempValue = alignedOperator.get(i);
+                if (tempValue.equals("*") || tempValue.equals("/") || tempValue.equals("-") || tempValue.equals("+")) {
+                    System.out.println("stringNumber1: " + stringNumber1 + " stringNumber2: " + stringNumber2);
+                    switch (tempValue) {
+                        case "*":
+                            tempResult = Float.parseFloat(stringNumber1) * Float.parseFloat(stringNumber2);
+                            break;
+                        case "/":
+                            tempResult = Float.parseFloat(stringNumber1) / Float.parseFloat(stringNumber2);
+                            break;
+                        case "-":
+                            tempResult = Float.parseFloat(stringNumber1) - Float.parseFloat(stringNumber2);
+                            break;
+                        case "+":
+                            tempResult = Float.parseFloat(stringNumber1) + Float.parseFloat(stringNumber2);
+                            break;
+                    }
+                    System.out.println(stringNumber1 + alignedOperator.get(i) + stringNumber2);
+                    alignedOperator.remove(i);
+                    alignedOperator.remove(stringNumber1);
+                    stringNumber1 = "";
+                    alignedOperator.remove(stringNumber2);
+                    stringNumber2 = "";
+                    alignedOperator.add(firstNumberPoint, "" + tempResult);
+                    firstNumberPoint = 0;
+                    System.out.println("=" + tempResult);
+                    if (alignedOperatorSize > 2) {
+                        alignedOperatorSize -= 2;
+                        i = -1;
+                    } else {
+                        break;
+                    }
+                } else {
+                    if (stringNumber1.equals("")) {
+                        stringNumber1 = alignedOperator.get(i);
+                        firstNumberPoint = i;
+                    } else if (stringNumber2.equals("")) {
+                        stringNumber2 = alignedOperator.get(i);
+                    }
+                }
+            }
+                String result = alignedOperator.get(0);
+                int resultLength = result.length();
+                if (resultLength > 2) {
+                    if (result.substring(resultLength - 2, resultLength).equals(".0")) {
+                        result = result.substring(0, resultLength - 2);
+                    }
+                }
+                String value;
+                value = result;
+                sendingText.setText(value);
+                isNumberPadClick = true;
+                backOperator.clear();
+                operationStack.clear();
+                alignedOperator.clear();
+                backOperator.add(value);
+            }
         }
-    }
+
     public void pointClicked(View v){
         if(isNumberPadClick && !isPointClick){
             backOperator.add(".");
             isPointClick = true;
             isNumberPadClick = false;
+            String value = sendingText.getText().toString();
+            value += ".";
+            sendingText.setText(value);
         }
     }
     public void numberPadClicked(View v) {
@@ -156,7 +221,7 @@ public class Calculator extends AppCompatActivity {
                     backOperator.add("+");
                     break;
             }
-            isPointClick = true;
+            isPointClick = false;
             isNumberPadClick = false;
             String value = sendingText.getText().toString();
             int backOperatorSize = backOperator.size()-1;
