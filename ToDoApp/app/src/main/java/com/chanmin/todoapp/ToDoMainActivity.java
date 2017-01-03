56da50a7f65968c887d1888ea7f1a2cb3645374b
jungchanmin <pgy6072@gmail.com>
2017-01-03 15:30:33 +0900

    데이터 저장기능추가, 리스트 수정기능추가, addlist -> cancle = 리스트 클리어.(아직 리스트 제거 미구현상태)

--- ToDoApp/app/src/main/java/com/chanmin/todoapp/ToDoMainActivity.java
+++ ToDoApp/app/src/main/java/com/chanmin/todoapp/ToDoMainActivity.java
@@ -2,9 +2,11 @@ package com.chanmin.todoapp;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
@@ -13,43 +15,84 @@ import android.widget.TextView;
 import java.util.ArrayList;
 
 public class ToDoMainActivity extends AppCompatActivity {
-ArrayList<String> titleList;
-ArrayAdapter<String> titleAdapter;
-ListView list;
-Button addNewListButton;
-int count;
-TextView fixedTitle;
-boolean isAddListCalledByUser;
-final static int MAIN = 0;
+    ArrayList<String> titleList;
+    ArrayAdapter<String> titleAdapter;
+    ListView list;
+    Button addNewListButton;
+    boolean isAddListCalledByUser;
+    final int MAIN = 0;
+    final int MAIN2 = 1;
+    final static String MAINKEY = "titleList";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_to_do_main);
-        fixedTitle = (TextView)(findViewById(R.id.fixedTitle));
-        String value = "To Do List";
-        fixedTitle.setText(value);
-        list = (ListView)findViewById(R.id.listView);
-        addNewListButton = (Button)findViewById(R.id.addNewList);
-        count = 0;
+        list = (ListView) findViewById(R.id.listView);
+        addNewListButton = (Button) findViewById(R.id.addNewList);
         titleList = new ArrayList<>();
+        SharedPreferences savedList = getSharedPreferences("toDoListTitle", 0);
+        int count = savedList.getInt("count", 0);
+        for (int i = 0; i < count; i++) {
+            titleList.add(savedList.getString("" + i, "error"));
+        }
         titleAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_multiple_choice, titleList);
         list.setAdapter(titleAdapter);
-
+        list.setOnItemClickListener(listViewClickListener);
     }
-    public void addNewListButtonClicked(View v){
+
+    public void addNewListButtonClicked(View v) {
         isAddListCalledByUser = true;
         Intent intent = new Intent(ToDoMainActivity.this, ToDoSubActivity.class);
-        startActivityForResult(intent,MAIN);
+        intent.putExtra("isReviseOrView", false);
+        startActivityForResult(intent, MAIN);
     }
-    protected void onActivityResult(int requestCode, int resultCode, Intent data){
-        switch(requestCode){
-            case MAIN :
-                if(resultCode == Activity.RESULT_OK){
-                    titleList.add(data.getStringExtra("titleList"));
+
+    AdapterView.OnItemClickListener listViewClickListener = new AdapterView.OnItemClickListener() {
+        public void onItemClick(AdapterView<?> parentView, View clickedView, int position, long id) {
+            String clickedTitle = ((TextView) clickedView).getText().toString();
+            Intent intent = new Intent(ToDoMainActivity.this, ToDoSubActivity.class);
+            intent.putExtra("clickedTitle", clickedTitle);
+            System.out.println("clickedTitle:" + clickedTitle);
+            intent.putExtra("isReviseOrView", true);
+            startActivityForResult(intent, MAIN2);
+        }
+    };
+
+    public void onPause() {
+        super.onPause();
+        int count = 0;
+        SharedPreferences toDoListTitle = getSharedPreferences("toDoListTitle", 0);
+        SharedPreferences.Editor edit = toDoListTitle.edit();
+        for (String list : titleList) {
+            edit.putString("" + count, list);
+            count++;
+        }
+        edit.putInt("count", count);
+        edit.apply();
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case MAIN:
+                if (resultCode == Activity.RESULT_OK) {
+                    titleList.add(data.getStringExtra(MAINKEY));
                     titleAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_multiple_choice, titleList);
                     list.setAdapter(titleAdapter);
+                    //titleAdapter.notifyDataSetChanged();
+                } else {
+                    titleList.clear();
                 }
+                break;
+            case MAIN2:
+                if (resultCode == Activity.RESULT_OK) {
+                    titleList.remove(data.getStringExtra("originalTitle"));
+                    titleList.add(data.getStringExtra(MAINKEY));
+                    titleAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_multiple_choice, titleList);
+                    list.setAdapter(titleAdapter);
+                    //titleAdapter.notifyDataSetChanged();
+                }
+                break;
         }
     }
 
--- ToDoApp/app/src/main/java/com/chanmin/todoapp/ToDoSubActivity.java
+++ ToDoApp/app/src/main/java/com/chanmin/todoapp/ToDoSubActivity.java
@@ -2,6 +2,7 @@ package com.chanmin.todoapp;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -14,35 +15,61 @@ import android.widget.TextView;
 import org.w3c.dom.Text;
 
 import java.util.ArrayList;
+import java.util.prefs.Preferences;
+
+import static com.chanmin.todoapp.ToDoMainActivity.MAINKEY;
 
 public class ToDoSubActivity extends AppCompatActivity {
-Button apply;
-Button cancel;
-EditText title;
-EditText main;
-TextView fixedTitle;
+    Button apply;
+    Button cancel;
+    EditText title;
+    EditText main;
+    TextView fixedTitle;
+    String value;
+    Boolean isReviseOrView;
+    String originalTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_to_do_sub);
-        fixedTitle = (TextView)findViewById(R.id.fixedTitle);
-        String value = "Add List";
+        fixedTitle = (TextView) findViewById(R.id.fixedTitle);
+        apply = (Button) findViewById(R.id.apply);
+        cancel = (Button) findViewById(R.id.cancel);
+        title = (EditText) findViewById(R.id.titleText);
+        main = (EditText) findViewById(R.id.mainText);
+        Intent intent = getIntent();
+        isReviseOrView = intent.getBooleanExtra("isReviseOrView", false);
+        originalTitle = intent.getStringExtra("clickedTitle");
+        System.out.println("originalTitle:" + originalTitle);
+        if (isReviseOrView) {
+            value = "View List";
+            title.setText(intent.getStringExtra("clickedTitle"));
+            apply.setText("complete");
+            SharedPreferences mainText = getPreferences(0);
+            main.setText(mainText.getString(intent.getStringExtra("clickedTitle"), "error"));
+        } else {
+            value = "Add List";
+        }
         fixedTitle.setText(value);
-        apply = (Button)findViewById(R.id.apply);
-        cancel = (Button)findViewById(R.id.cancel);
-        title = (EditText)findViewById(R.id.titleText);
-        main = (EditText)findViewById(R.id.mainText);
     }
-    public void cancelButtonClicked(View v){
+
+    public void cancelButtonClicked(View v) {
         setResult(RESULT_CANCELED);
         finish();
     }
-    public void applyButtonClicked(View v){
+
+    public void applyButtonClicked(View v) {
         String value = title.getText().toString();
+        String mainTextValue = main.getText().toString();
+        SharedPreferences mainText = getPreferences(0);
+        SharedPreferences.Editor edit = mainText.edit();
+        edit.putString(value, mainTextValue);
+        edit.commit();
         Intent intent = new Intent();
-        intent.putExtra("titleList",value);
-        setResult(Activity.RESULT_OK,intent);
+        intent.putExtra("" + MAINKEY, value);
+        intent.putExtra("originalTitle", originalTitle);
+        setResult(Activity.RESULT_OK, intent);
         finish();
     }
 }
