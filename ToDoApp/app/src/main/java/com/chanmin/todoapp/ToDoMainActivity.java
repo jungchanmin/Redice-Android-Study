351d618ccf4c10089060c925069aecc5da1b1be4
jungchanmin <pgy6072@gmail.com>
2017-01-05 18:06:10 +0900

    important버튼 기능추가, delete버튼 기능 추가. 메모 저장여부 확인  checkbox,listTitle클릭 이벤트 분리 실패

--- ToDoApp/app/src/main/java/com/chanmin/todoapp/ToDoMainActivity.java
+++ ToDoApp/app/src/main/java/com/chanmin/todoapp/ToDoMainActivity.java
@@ -5,10 +5,13 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.SparseBooleanArray;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CheckedTextView;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -16,9 +19,11 @@ import java.util.ArrayList;
 
 public class ToDoMainActivity extends AppCompatActivity {
     ArrayList<String> titleList;
+    ArrayList<Boolean> clickList;
     ArrayAdapter<String> titleAdapter;
     ListView list;
     Button addNewListButton;
+    Button deleteButton;
     boolean isAddListCalledByUser;
     final int MAIN = 0;
     final int MAIN2 = 1;
@@ -28,8 +33,10 @@ public class ToDoMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_to_do_main);
-        list = (ListView) findViewById(R.id.listView);
         addNewListButton = (Button) findViewById(R.id.addNewList);
+        deleteButton = (Button) findViewById(R.id.delete);
+        list = (ListView) findViewById(R.id.listView);
+        clickList = new ArrayList<>();
         titleList = new ArrayList<>();
         SharedPreferences savedList = getSharedPreferences("toDoListTitle", 0);
         int count = savedList.getInt("count", 0);
@@ -39,6 +46,11 @@ public class ToDoMainActivity extends AppCompatActivity {
         titleAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_multiple_choice, titleList);
         list.setAdapter(titleAdapter);
         list.setOnItemClickListener(listViewClickListener);
+        list.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+        list.clearChoices();
+        for (int i = 0; i < titleList.size(); i++) {
+            clickList.add(false);
+        }
     }
 
     public void addNewListButtonClicked(View v) {
@@ -48,12 +60,24 @@ public class ToDoMainActivity extends AppCompatActivity {
         startActivityForResult(intent, MAIN);
     }
 
+    public void deleteButtonClicked(View v) {
+        System.out.println("list.getCheckedItemPosition() : " + list.getCheckedItemPosition());
+        SparseBooleanArray isCheck = list.getCheckedItemPositions();
+        int count = titleAdapter.getCount();
+        for (int i = count - 1; i >= 0; i--) {
+            if (isCheck.get(i)) {
+                titleList.remove(i);
+            }
+        }
+        list.clearChoices();
+        titleAdapter.notifyDataSetChanged();
+    }
+
     AdapterView.OnItemClickListener listViewClickListener = new AdapterView.OnItemClickListener() {
         public void onItemClick(AdapterView<?> parentView, View clickedView, int position, long id) {
             String clickedTitle = ((TextView) clickedView).getText().toString();
             Intent intent = new Intent(ToDoMainActivity.this, ToDoSubActivity.class);
             intent.putExtra("clickedTitle", clickedTitle);
-            System.out.println("clickedTitle:" + clickedTitle);
             intent.putExtra("isReviseOrView", true);
             startActivityForResult(intent, MAIN2);
         }
@@ -62,6 +86,14 @@ public class ToDoMainActivity extends AppCompatActivity {
     public void onPause() {
         super.onPause();
         int count = 0;
+        while (true) {
+            int listCheck = list.getCheckedItemPosition();
+            if (listCheck != ListView.INVALID_POSITION) {
+                titleList.remove(listCheck);
+                continue;
+            }
+            break;
+        }
         SharedPreferences toDoListTitle = getSharedPreferences("toDoListTitle", 0);
         SharedPreferences.Editor edit = toDoListTitle.edit();
         for (String list : titleList) {
@@ -76,21 +108,28 @@ public class ToDoMainActivity extends AppCompatActivity {
         switch (requestCode) {
             case MAIN:
                 if (resultCode == Activity.RESULT_OK) {
-                    titleList.add(data.getStringExtra(MAINKEY));
-                    titleAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_multiple_choice, titleList);
-                    list.setAdapter(titleAdapter);
-                    //titleAdapter.notifyDataSetChanged();
+                    if (data.getBooleanExtra("isImportant", false)) {
+                        titleList.add(0, data.getStringExtra(MAINKEY));
+                        list.clearChoices();
+                    } else {
+                        titleList.add(data.getStringExtra(MAINKEY));
+                    }
+                    titleAdapter.notifyDataSetChanged();
                 } else {
                     titleList.clear();
                 }
                 break;
             case MAIN2:
                 if (resultCode == Activity.RESULT_OK) {
+                    int position = titleList.indexOf(data.getStringExtra("originalTitle"));
                     titleList.remove(data.getStringExtra("originalTitle"));
-                    titleList.add(data.getStringExtra(MAINKEY));
-                    titleAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_multiple_choice, titleList);
-                    list.setAdapter(titleAdapter);
-                    //titleAdapter.notifyDataSetChanged();
+                    if (data.getBooleanExtra("isImportant", false)) {
+                        titleList.add(0, data.getStringExtra(MAINKEY));
+                        list.clearChoices();
+                    } else {
+                        titleList.add(position, data.getStringExtra(MAINKEY));
+                    }
+                    titleAdapter.notifyDataSetChanged();
                 }
                 break;
         }
--- ToDoApp/app/src/main/java/com/chanmin/todoapp/ToDoSubActivity.java
+++ ToDoApp/app/src/main/java/com/chanmin/todoapp/ToDoSubActivity.java
@@ -5,12 +5,15 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
@@ -27,7 +30,9 @@ public class ToDoSubActivity extends AppCompatActivity {
     TextView fixedTitle;
     String value;
     Boolean isReviseOrView;
+    Boolean important;
     String originalTitle;
+    CheckBox importantChecking;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,16 +43,22 @@ public class ToDoSubActivity extends AppCompatActivity {
         cancel = (Button) findViewById(R.id.cancel);
         title = (EditText) findViewById(R.id.titleText);
         main = (EditText) findViewById(R.id.mainText);
+        importantChecking = (CheckBox) findViewById(R.id.importantCheckbox);
         Intent intent = getIntent();
         isReviseOrView = intent.getBooleanExtra("isReviseOrView", false);
         originalTitle = intent.getStringExtra("clickedTitle");
+        important = false;
         System.out.println("originalTitle:" + originalTitle);
         if (isReviseOrView) {
             value = "View List";
             title.setText(intent.getStringExtra("clickedTitle"));
             apply.setText("complete");
+            String titleText = title.getText().toString();
+            Log.i("testingString", titleText);
             SharedPreferences mainText = getPreferences(0);
-            main.setText(mainText.getString(intent.getStringExtra("clickedTitle"), "error"));
+            main.setText(mainText.getString(titleText, "error"));
+            important = mainText.getBoolean(titleText + "important", false);
+            importantChecking.setChecked(important);
         } else {
             value = "Add List";
         }
@@ -59,16 +70,36 @@ public class ToDoSubActivity extends AppCompatActivity {
         finish();
     }
 
+    public void checking(View v) {
+        Boolean checkingValue = importantChecking.isChecked();
+        if (checkingValue) {
+            important = true;
+        } else {
+            important = false;
+        }
+    }
+
     public void applyButtonClicked(View v) {
         String value = title.getText().toString();
+        if (important) {
+            value += "*";
+        } else {
+            int valueLength = value.length();
+            String importantValue = value.substring(valueLength - 1, valueLength);
+            if (importantValue.equals("*")) {
+                value = value.substring(0, valueLength - 1);
+            }
+        }
         String mainTextValue = main.getText().toString();
         SharedPreferences mainText = getPreferences(0);
         SharedPreferences.Editor edit = mainText.edit();
         edit.putString(value, mainTextValue);
+        edit.putBoolean(value + "important", important);
         edit.commit();
         Intent intent = new Intent();
         intent.putExtra("" + MAINKEY, value);
         intent.putExtra("originalTitle", originalTitle);
+        intent.putExtra("isImportant", important);
         setResult(Activity.RESULT_OK, intent);
         finish();
     }
